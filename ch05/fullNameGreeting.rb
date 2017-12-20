0441e1699fc27bff0f57914b767465c0b5ce36b5
g.fabro <g.fabro@berkeley.edu>
2017-12-19 22:51:16 -0800

    fullNameGreeting

---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts 'Hi user! What is your first name?'
+firstName = gets.chomp
+puts 'What is your middle name?'
+middleName = gets.chomp
+puts 'Now, what is your last name?'
+lastName= gets.chomp
+puts 'Greetings ' + firstName + ' ' + middleName + ' ' + lastName + '! Lovely to meet you.'
