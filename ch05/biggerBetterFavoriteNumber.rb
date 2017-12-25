68147eba19eeb32f66d5c59ae51cdd69019dac42
g.fabro <g.fabro@berkeley.edu>
2017-12-25 14:51:11 -0800

    biggerBetterFavoriteNumber

--- ch05/biggerBetterFavoriteNumber.rb
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -1,5 +1,4 @@
 puts 'Hi user! What is your favorite number?'
 favNumber = gets.chomp
-add = 1
 name = favNumber.to_i + 1
 puts 'That is a wonderful number, but a bigger and better favorite number should be ' + name.to_s + '. :)'
