b14dec8ddcb9dc69682f092158ebc0e99ca40d7c
g.fabro <g.fabro@berkeley.edu>
2017-12-29 11:20:50 -0800

    tableOfContents Arrays

--- ch08/tableOfContents.rb
+++ ch08/tableOfContents.rb
@@ -1,10 +1,8 @@
 chapNames = ['Chapter 1: Getting Started', 'Chapter 2: Numbers        ', 'Chapter 3: Letters        ']
 pageNum =   ['                   page  1', '                   page  9', '                   page 13']
-
 lineWidth = 57
 puts(   'Table of Contents         '.center(lineWidth))
 puts ''
-
 puts chapNames[0].ljust(lineWidth/3) + pageNum[0].rjust(lineWidth/3)
 puts chapNames[1].ljust(lineWidth/3) + pageNum[1].rjust(lineWidth/3)
 puts chapNames[2].ljust(lineWidth/3) + pageNum[2].rjust(lineWidth/3)
