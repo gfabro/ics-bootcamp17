551909dca5041342136797997a686a7a31a2fd63
g.fabro <g.fabro@berkeley.edu>
2018-01-09 13:21:29 -0800

    shuffle.rb Recursive

--- ch10/shuffle.rb
+++ ch10/shuffle.rb
@@ -2,21 +2,22 @@ def shuffle some_array
   recursive_shuffle some_array, []
 end
 def recursive_shuffle unshuffled_array, shuffled_array
-    while unshuffled_array.length > 0
-        random_array = rand(unshuffled_array.length)
-        counter = 0
-        updated_array = []
-        unshuffled_array.each do |check|
-          if(counter == random_array)
-            shuffled_array.push check
-          else
-            updated_array.push check
-          end
-            counter += 1
-        end
-        unshuffled_array = updated_array
+  if(unshuffled_array.length <= 0) #if only one word is entered
+    return shuffled_array.push unshuffled_array
+  end
+  random_array = rand(unshuffled_array.length)
+  counter = 0
+  updated_array = []
+  unshuffled_array.each do |check|
+    if(counter == random_array)
+      shuffled_array.push check
+    else
+      updated_array.push check
     end
-    shuffled_array
+      counter += 1
+  end
+  shuffled_array 
+  recursive_shuffle updated_array, shuffled_array
 end
 puts 'Write words you want to be shuffled.'
 list = []
